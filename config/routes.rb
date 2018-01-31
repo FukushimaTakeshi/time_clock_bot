d78f5ff6ed23180e4b6b42d2031d49052a1eaffd
FukushimaTakeshi <whippet.818@gmail.com>
2018-01-31 20:50:39 +0900

    polish routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
-  get '/:id/signup', to: 'users#new'
-  # post '/signup', to: 'users#create'
-  patch '/:id/signup', to: 'users#update', as: 'signup'
+  get '/signup/:id', to: 'users#new'
+  patch '/signup/:id', to: 'users#update', as: 'signup'
 
   resources :users
   resources :account_activations, param: :token, only: [:edit]
