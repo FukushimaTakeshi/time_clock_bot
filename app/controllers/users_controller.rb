479208aa6e67ce1704e8861c2c6843ab77e496ff
FukushimaTakeshi <whippet.818@gmail.com>
2018-02-01 21:35:35 +0900

    support bootstrap

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
       flash[:success] = "Profile updated"
       redirect_to @user
     else
-      render 'edit'
+      render 'new'
     end
   end
 
