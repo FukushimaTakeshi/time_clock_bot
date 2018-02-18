63148a7a2d6c789a3823350705e5b7efdacb52fa
FukushimaTakeshi <whippet.818@gmail.com>
2018-02-18 22:08:30 +0900

    add error log

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,11 +9,13 @@ class ApplicationController < ActionController::Base
     rescue_from ActiveRecord::RecordNotFound, with: :render_404
   end
 
-  def render_404
+  def render_404(e = nil)
+    logger.error("#{e.class} (#{e.message}):\n#{e.backtrace.join("\n")}") if e    
     render file: Rails.root.join('public/404.html'), status: 404, layout: false, content_type: 'text/html'
   end
 
-  def render_500
+  def render_500(e = nil)
+    logger.error("#{e.class} (#{e.message}):\n#{e.backtrace.join("\n")}") if e
     render file: Rails.root.join('public/500.html'), status: 500, layout: false, content_type: 'text/html'
   end
 
