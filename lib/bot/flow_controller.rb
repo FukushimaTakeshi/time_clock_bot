f8bcb1dab9c8b7096c0c0fef60b5104833c2d2d8
FukushimaTakeshi <whippet.818@gmail.com>
2018-02-18 19:16:56 +0900

    clean flow controller

--- lib/bot/flow_controller.rb
+++ lib/bot/flow_controller.rb
@@ -8,8 +8,7 @@ module Bot
 
     def flow(event)
       @memory ||= Rails.cache.read(line_user_id)
-      p '----------memory-----------'
-      p @memory
+      Rails.logger.info(@memory)
 
       if @memory.blank?
         api_ai = Bot::Flow::Base.parse_intent(event)
@@ -19,20 +18,25 @@ module Bot
           to_confirme: {}, # 確認するslot
           confirming: nil, # 現在確認中のslot
           verified: {
-            confirmed: []
+            confirmed: [] # 最終返答済みのslot
           }
         }
         # "First"
-        # 初期状態 1.意図を抽出 2.slotを回収 3.すべてのslotを回収できれば最終解答、出来なければ確認メッセージ返答
-        p '----Firest Flow----'
+        # 初期状態、ここからスタート
+        #  1.意図を抽出
+        #  2.slotを回収
+        #  3.すべてのslotを回収できれば最終解答、出来なければslot回収メッセージ返答
+        Rails.logger.info('--Firest Flow--')
         bot_flow = Bot::Flow::First.new(event, @memory)
         messages = bot_flow.run
 
       else
-        # "Reply"
-        # 確認中のslotがある状態 1.メッセージからslotを回収 2.すべてを回収できれば最終解答、出来なければ確認メッセージ返答
-        p '----Reply Flow----'
         if @memory[:confirming].present?
+          # "Reply"
+          # 確認中のslotがある状態
+          #  1.メッセージからslotを回収
+          #  2.すべてを回収できれば最終解答、出来なければ確認メッセージ返答
+          Rails.logger.info('--Reply Flow--')
           bot_flow = Bot::Flow::Reply.new(event, @memory)
           messages = bot_flow.run
 
@@ -41,38 +45,29 @@ module Bot
           if api_ai.dig(:result, :action) != 'input.unknown'
             @memory[:intent] = api_ai[:result]
 
+            @memory[:confirmed] = {}
             # Change intent
             # 一度、最終返答済みの状態から意図が変更された状態
             # Firstと同様だが、収集したパラメータを継承している
-            p '----Change Intent Flow----'
-            @memory[:confirmed] = {}
+            Rails.logger.info('--Change Intent Flow--')
             bot_flow = Bot::Flow::First.new(event, @memory)
             messages = bot_flow.run
 
           else
-            # "Change Slot"
-            # 一度、最終返答済みの状態からslotが変更された状態
-            # p '----Change Slot Flow----'
-            # if @memory[:verified][:confirmed].present?
-            #   bot_flow = Bot::Flow::ChangeSlot.new(event, @memory)
-            #   messages = bot_flow.run
-            #
-            # else
-              # "Other"
-              # 一度、最終返答済みであが、上記のflowのどれにも当てはまらない場合
-              @memory = {
-                intent: api_ai[:result],
-                confirmed: {},
-                to_confirme: {},
-                confirming: nil,
-                verified: {
-                  confirmed: []
-                }
+            # "Other"
+            # 一度、最終返答済みであが、上記のflowのどれにも当てはまらない場合
+            @memory = {
+              intent: api_ai[:result],
+              confirmed: {},
+              to_confirme: {},
+              confirming: nil,
+              verified: {
+                confirmed: []
               }
-              p '----Other Flow----'
-              bot_flow = Bot::Flow::Other.new(event, @memory)
-              messages = bot_flow.run
-            # end
+            }
+            Rails.logger.info('--Other Flow--')
+            bot_flow = Bot::Flow::Other.new(event, @memory)
+            messages = bot_flow.run
           end
         end
       end
