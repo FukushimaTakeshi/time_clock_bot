5042ad9addce1acb3fd91d8f73d88325e9fb6f9b
FukushimaTakeshi <whippet.818@gmail.com>
2018-03-25 21:16:22 +0900

    wip spec bot flow-controller

--- spec/lib/bot/flow_controller_spec.rb
+++ spec/lib/bot/flow_controller_spec.rb
@@ -7,124 +7,129 @@ WebMock.allow_net_connect!
 
 RSpec.describe 'FolowController' do
 
-  describe 'flow' do
-    let(:api_body) do
-      {
-        id: '9f071bc4-ae2d-4819-a7d5-f395140a9c53',
-        timestamp: '2018-03-22T11:46:21.321Z',
-        lang: 'ja',
-        result: {
-          source: 'agent',
-          resolvedQuery: '登録する',
-          action: 'register',
-          actionIncomplete: false,
-          parameters: {
-            time: '',
-            time1: '',
-            Chronus: 'chronus'
-          },
-          contexts: [],
-          metadata: {
-            intentId: '9e0cc0e6-38b5-4412-a68e-07356f478a1a',
-            webhookUsed: 'false',
-            webhookForSlotFillingUsed: 'false',
-            intentName: '登録'
-          },
-          fulfillment: {
-            speech: '',
-            messages: [
-              {
-                type: 0,
-                speech: ''
-              }
-            ]
-          },
-          score: 0.7400000095367432
-        },
-        status: {
-          code: 200,
-          errorType: 'success',
-          webhookTimedOut: false
-        },
-        sessionId: 'fc629ed1-20fe-4611-9fe7-1e4a3aeb2214'
-      }
-    end
-    before do
-      stub_request(:post, 'https://api.api.ai/v1/').to_return { |request| { body: api_body.to_json, status: 200 } }
-    end
-    describe 'First Flow' do
-      let(:memory) do
-        # {
-        #   intent: api_ai[:result],
-        #   confirmed: {},
-        #   to_confirme: {},
-        #   confirming: nil,
-        #   verified {
-        #     confirmed: []
-        #   }
-        # }
-      end
+  let(:memory) do
+    # {
+    #   intent: api_ai[:result],
+    #   confirmed: {},
+    #   to_confirme: {},
+    #   confirming: nil,
+    #   verified {
+    #     confirmed: []
+    #   }
+    # }
+  end
+  let(:response_headers) do
+    { "Content-Type" => "application/json;charset=UTF-8", "Content-Length" => "437", "Connection" => "close", "Access-Control-Allow-Credentials" => "true", "Cache-Control" => "no-cache=\"set-cookie\"", "Date" => "Wed, 12 Oct 2016 20:07:54 GMT", "Server" => "nginx/1.9.7", "Set-Cookie" => "AWSELB=9D5B4D210CCFFAF1BE1E0CD7C7E6FCBD7B46140CAAF64A202A005B9079598B549F7A5EC269DD0FF88508DA57410EFC7882B7860453691E7ACC870186C9D1589D2A332B51EC;PATH=/", "X-Cache" => "Miss from cloudfront", "Via" => "1.1 978198446b6fdba8a499c04f84a3a7e6.cloudfront.net (CloudFront)", "X-Amz-Cf-Id" => "ilwhpG75Ea4iXumklw7484nYt2jbx-L6ZaeiO9naUOstx45ia_nuaQ==" }
+  end
 
-      let(:line_request) do
-        {
-          events: [{
-            type: 'message',
-            replyToken: "9a68727b1ee34d84a7c1323549dc2cce",
-            source: {
-              userId: "U11dcf2ab1d8608c6a",
-              type: "user"
-            },
-            timestamp: 1521286274866,
-            message: {
-              type: "text",
-              id: "7639986520165",
-              text: "逋ｻ骭ｲ縺吶ｋ"
-            } }],
-          webhook: {
-            events: [{
-              type: "message",
-              replyToken: "9a68727b1ee34d84a7c1323549dc2cce",
-              source: {
-                userId: "U11dcf2ab1d8608c6a",
-                type: "user"
-              },
-              timestamp: 1521286274866,
-              message: {
-                type: "text",
-                id: "7639986520165",
-                text: "登録する"
-              }
-            }]
+  let(:line_request) do
+    {
+      events: [{
+        type: 'message',
+        replyToken: '9a68727b1ee34d84a7c1323549dc2cce',
+        source: {
+          userId: 'U11dcf2ab1d8608c6a',
+          type: 'user'
+        },
+        timestamp: 1521286274866,
+        message: {
+          type: 'text',
+          id: '7639986520165',
+          text: '登録する'
+        } }],
+      webhook: {
+        events: [{
+          type: 'message',
+          replyToken: '9a68727b1ee34d84a7c1323549dc2cce',
+          source: {
+            userId: 'U11dcf2ab1d8608c6a',
+            type: 'user'
+          },
+          timestamp: 1521286274866,
+          message: {
+            type: 'text',
+            id: '7639986520165',
+            text: '登録する'
           }
-        }
-      end
+        }]
+      }
+    }
+  end
 
-      before do
-        # Rails.cache.write('test123', memory)
-      end
-
-      it '' do
+  describe 'flow' do
+    describe 'First Flow' do
+      before { Rails.cache.clear }
+      let(:event) do
         line_client = Line::Bot::Client.new
-        event = line_client.parse_events_from(line_request.to_json)[0]
-        messages = Bot::FlowController.new('test123').flow(event)
-        p messages
-        p '--------------------------'
-        expect(messages).to eq(
-          {
-            type: 'template',
-            altText: '始業時間は何時ですか？',
-            template: {
-              type: 'buttons',
-              text: "始業時間は何時ですか？\n当てはまらない場合は4桁の数字でコメントしてね。",
-              actions: [
-                { type: 'postback', label: '9:00', data: '0900', displayText: '0900' },
-                { type: 'postback', label: '10:00', data: '1000', displayText: '1000' },
-                { type: 'postback', label: '10:30', data: '1030', displayText: '1030' },
-                { type: 'postback', label: '11:00', data: '1100', displayText: '1100' }
-              ]
+        line_client.parse_events_from(line_request.to_json)[0]
+      end
+
+      context 'intentを抽出できた場合' do
+        before do
+          stub_request(:post, "https://api.api.ai/v1/query?v=20150910").
+            to_return(status: 200, body: File.new("#{Rails.root}/spec/fixtures/files/identify_intent_success.json"), headers: response_headers)
+        end
+
+        it 'template(始業時間)を返す' do
+          messages = Bot::FlowController.new('test123').flow(event)
+          expect(messages).to eq(
+            {
+              type: 'template',
+              altText: '始業時間は何時ですか？',
+              template: {
+                type: 'buttons',
+                text: "始業時間は何時ですか？\n当てはまらない場合は4桁の数字でコメントしてね。",
+                actions: [
+                  { type: 'postback', label: '9:00', data: '0900', displayText: '0900' },
+                  { type: 'postback', label: '10:00', data: '1000', displayText: '1000' },
+                  { type: 'postback', label: '10:30', data: '1030', displayText: '1030' },
+                  { type: 'postback', label: '11:00', data: '1100', displayText: '1100' }
+                ]
+              }
             }
-          }.to_json
-        )
+          )
+        end
+      end
+
+      context 'intentとparameter(time)を抽出できた場合' do
+        before do
+          stub_request(:post, "https://api.api.ai/v1/query?v=20150910").
+            to_return(status: 200, body: File.new("#{Rails.root}/spec/fixtures/files/identify_intent_parameter1_success.json"), headers: response_headers)
+        end
+
+        it 'template(就業時間)を返す' do
+          messages = Bot::FlowController.new('test123').flow(event)
+          expect(messages).to eq(
+            {
+              type: 'template',
+              altText: '就業時間は何時ですか？',
+              template: {
+                type: 'buttons',
+                text: "就業時間は何時ですか？\n当てはまらない場合は4桁の数字でコメントしてね。",
+                actions: [
+                  { type: 'postback', label: '17:45', data: '1745', displayText: '1745' },
+                  { type: 'postback', label: '19:00', data: '1900', displayText: '1900' },
+                  { type: 'postback', label: '19:30', data: '1930', displayText: '1930' },
+                  { type: 'postback', label: '20:00', data: '2000', displayText: '2000' }
+                ]
+              }
+            }
+          )
+        end
+      end
+
+      context 'intentとparameter(time, time1)を抽出できた場合' do
+        before do
+          stub_request(:post, "https://api.api.ai/v1/query?v=20150910").
+            to_return(status: 200, body: File.new("#{Rails.root}/spec/fixtures/files/identify_intent_parameters_success.json"), headers: response_headers)
+
+          allow(Register).to receive(:new).and_return(Register.new)
+          allow(Register.new).to receive(:create_message).and_return('test_message')
+        end
+        it '登録メソッドが呼び出される' do
+          messages = Bot::FlowController.new('test123').flow(event)
+          expect(messages).to eq('test_message')
+        end
       end
     end
   end
